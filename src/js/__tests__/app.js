2fc76f18e90587151dd370dcda4571fb8eeabc95
lan-mak <lan-mak@ya.ru>
2022-05-16 00:05:04 +0300

    ver 0.01

---
+++ src/js/__tests__/app.js
@@ -0,0 +1,25 @@
+import specialInfo from '../app';
+
+test('id действия существует', () => {
+  const special = [
+    8,
+    'Двойной выстрел',
+    'http://...',
+    'Двойной выстрел наносит двойной урон',
+  ];
+  expect(specialInfo(8)).toEqual(special);
+});
+
+test('id действия существует и описание "засекречено"', () => {
+  const special = [
+    9,
+    'Нокаутирующий удар',
+    'http://...',
+    'Описание недоступно',
+  ];
+  expect(specialInfo(9)).toEqual(special);
+});
+
+test('id действия существует и описание "засекречено"', () => {
+  expect(specialInfo(10)).not.toBeTruthy();
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,39 @@
-console.log('hello world');
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+export default function specialInfo(id) {
+  const { special: [...data] } = character;
+  const itemId = id;
+  let arrayData;
+
+  data.forEach((item) => {
+    if (item.id === itemId) {
+      const {
+        id: specialId, name, icon, description = 'Описание недоступно',
+      } = item;
+      arrayData = [specialId, name, icon, description];
+    }
+  });
+
+  return arrayData;
+}
